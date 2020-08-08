3372aab7696bd0adaa8e20e070e8c777676e3290
Carlos Tojal <carlos.tojal@hotmail.com>
2020-08-08 15:08:54 +0100

    a new start

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const { ApolloServer } = require("apollo-server");
-const { typedefs } = require("./src/typedefs.js");
-const { resolvers } = require("./src/resolvers");
 require("dotenv").config();
 
+/*
 const server = new ApolloServer({
     typeDefs: typedefs,
     resolvers: resolvers
@@ -10,4 +9,4 @@ const server = new ApolloServer({
 
 server.listen().then(({ url }) => {
     console.log(`GraphQL Playground running at ${url}`);
-});
+});*/
