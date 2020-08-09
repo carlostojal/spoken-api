b3f7b96c552e16589a19b84a2d9cd5567fea089d
Carlos Tojal <carlos.tojal@hotmail.com>
2020-08-09 13:12:26 +0100

    login security improvements/fixes

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -9,30 +9,42 @@ exports.resolvers = {
       return new Promise((resolve, reject) => {
         User.findOne({
           username: args.username,
-          password: args.password
         }).then((result) => {
           if(result) { // user exists
 
-            // create tokens with specified duration and user id
-            const refresh_token = createToken(result._id, "refresh");
-            const access_token = createToken(result._id, "access");
+            if(result.email_confirmed) { // user confirmed the email, so can login
 
-            User.updateOne({ _id: result._id }, { $set: { // set tokens in database
-              access_token: access_token,
-              refresh_token: refresh_token
-            }}).then((updateResult) => {
-              // set httpOnly cookie containing the refresh token
-              context.res.cookie("refresh_token", refresh_token.value, {
-                expires: new Date(refresh_token.expiry),
-                httpOnly: true
+              bcrypt.compare(args.password, result.password, (err, compareSuccess) => {
+                if (err) reject(err);
+
+                if(compareSuccess) { // correct password
+                  // create tokens with specified duration and user id
+                  const refresh_token = createToken(result._id, "refresh");
+                  const access_token = createToken(result._id, "access");
+
+                  User.updateOne({ _id: result._id }, { $set: { // set tokens in database
+                    access_token: access_token,
+                    refresh_token: refresh_token
+                  }}).then((updateResult) => {
+                    // set httpOnly cookie containing the refresh token
+                    context.res.cookie("refresh_token", refresh_token.value, {
+                      expires: new Date(refresh_token.expiry),
+                      httpOnly: true
+                    });
+                    console.log("User got tokens.");
+                    resolve(access_token.value);
+                  }).catch((error) => {
+                    reject(error);
+                  });
+                } else { // user exists but password is not correct
+                  reject(new Error("Wrong password."));
+                }
               });
-              console.log("User got tokens.");
-              resolve(access_token.value);
-            }).catch((error) => {
-              reject(error);
-            });
+            } else {
+              reject(new Error("Email not confirmed."));
+            }
           } else {
-            reject(new Error("Bad credentials.")); 
+            reject(new Error("User not existent."));
           }
         }).catch((error) => {
           reject(error);
