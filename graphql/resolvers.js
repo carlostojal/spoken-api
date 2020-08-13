1994204c27bd8f118bd7f3aaa30bad617a3304aa
Carlos Tojal <carlos.tojal@hotmail.com>
2020-08-13 17:05:37 +0100

    get token in separate file/function

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,76 +1,15 @@
 const bcrypt = require("bcrypt");
-const createToken = require("../helpers/createToken");
+const { AuthenticationError } = require("apollo-server");
+const getToken = require("../helpers/getToken");
 const User = require("../models/User");
 const Post = require("../models/Post");
 const FollowRelation = require("../models/FollowRelation");
-const { AuthenticationError } = require("apollo-server");
-const followRelation = require("../schemas/FollowRelation");
-const { aggregate } = require("../models/User");
 
 exports.resolvers = {
   Query: {
     // get user tokens from username and password
     getToken: (parent, args, context, info) => {
-      return new Promise((resolve, reject) => {
-        User.findOne({
-          username: args.username,
-        }).then((user) => {
-          if(user) { // user exists
-
-            if(user.email_confirmed) { // user confirmed the email, so can login
-
-              bcrypt.compare(args.password, user.password, (err, compareSuccess) => {
-                if (err) reject(err);
-
-                if(compareSuccess) { // correct password
-                  // create tokens with specified duration and user id
-                  const refresh_token = createToken(user._id, "refresh");
-                  const access_token = createToken(user._id, "access");
-
-                  // remove expired refresh tokens
-                  for(let i = 0; i < user.refresh_tokens.length; i++) {
-                    if(user.refresh_tokens[i].expiry < Date.now()) {
-                      user.refresh_tokens.splice(i, 1);
-                      i--;
-                    }
-                  }
-
-                  // remove expired access tokens
-                  for(let i = 0; i < user.access_tokens.length; i++) {
-                    if(user.access_tokens[i].expiry < Date.now()) {
-                      user.access_tokens.splice(i, 1);
-                      i--;
-                    }
-                  }
-
-                  user.refresh_tokens.push(refresh_token);
-                  user.access_tokens.push(access_token);
-
-                  user.save().then((res) => {
-                    // send refresh token as httpOnly cookie
-                    context.res.cookie("refresh_token", refresh_token.value, {
-                      expires: new Date(refresh_token.expiry),
-                      httpOnly: true
-                    });
-                    console.log("User got tokens.");
-                    resolve(access_token.value); // send access token as query response
-                  }).catch((error) => {
-                    reject(error);
-                  });
-                } else { // user exists but password is not correct
-                  reject(new Error("Wrong password."));
-                }
-              });
-            } else {
-              reject(new Error("Email not confirmed."));
-            }
-          } else {
-            reject(new Error("User not existent."));
-          }
-        }).catch((error) => {
-          reject(error);
-        });
-      });
+      return getToken(args.username, args.password, context);
     },
 
     // get user data from ID or for the current user
