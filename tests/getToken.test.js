b4663fc4dc89fe9d3034097e3baf43c4788292fa
Carlos Tojal <carlos.tojal@hotmail.com>
2020-10-25 00:54:07 +0100

    more tests

--- tests/getToken.test.js
+++ tests/getToken.test.js
@@ -3,6 +3,14 @@ require("dotenv").config({ path: ".env" });
 const mysqlClient = require("../src/config/mysql");
 const redisClient = require("../src/config/redis");
 
-test("User authentication", () => {
-  return expect(getToken("test", "test", null, null, null, mysqlClient, redisClient)).rejects.toThrowError(new Error("USER_NOT_FOUND"));
+test("Get token for user not existent", () => {
+  return expect(getToken("i_dont_exists", "test", null, null, null, mysqlClient, redisClient)).rejects.toThrowError(new Error("USER_NOT_FOUND"));
+});
+
+test("Get token with wrong password", () => {
+  return expect(getToken("test", "test", null, null, null, mysqlClient, redisClient)).rejects.toThrowError(new Error("WRONG_PASSWORD"));
+});
+
+test("Get token with right password", () => {
+  return expect(getToken("test", "@Password123", null, null, null, mysqlClient, redisClient)).resolves.toBeDefined();
 });
