6866000079c8103cbd4544d8c710664079527fb9
Carlos Tojal <carlos.tojal@hotmail.com>
2020-12-26 15:13:30 +0000

    bug fix

--- src/helpers/jobs/mediaCleanup.js
+++ src/helpers/jobs/mediaCleanup.js
@@ -4,7 +4,6 @@ const mediaCleanup = () => {
     return new Promise(async (resolve, reject) => {
         let media = null;
         try {
-            let media = await getExpiredMedia();
             media = await getExpiredMedia();
             console.log(media);
             // remove the files one by one and remove from the database here
