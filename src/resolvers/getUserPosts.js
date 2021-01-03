456b6146e6e5c58b5ad17ea77693b224be3224ad
Carlos Tojal <carlos.tojal@hotmail.com>
2021-01-03 18:20:54 +0000

    bugfix

--- src/resolvers/getUserPosts.js
+++ src/resolvers/getUserPosts.js
@@ -1,5 +1,6 @@
 const { AuthenticationError } = require("apollo-server");
 
+const getUserById = require("../helpers/controllers/users/getUserById");
 const userFollowsUser = require("../helpers/controllers/users/userFollowsUser");
 const getPosts = require("../helpers/controllers/posts/getUserPosts");
 const formatPost = require("../helpers/formatPost");
@@ -12,15 +13,27 @@ const getUserPosts = (page, perPage, user_id, user) => {
 
     // if a user id was provided
     if(user_id) {
-      let follows = false;
+
+      let user1;
       try {
-        follows = await userFollowsUser(user.id, user_id);
+        user1 = await getUserById(user_id);
       } catch(e) {
-        return reject(new Error("ERROR_CHECKING_PERMISSION"));
+        return reject(new Error("ERROR_GETTING_USER"));
       }
 
-      if(!follows)
-        return reject(new Error("NOT_ALLOWED"));
+      // if the account is private, check if the user follows
+      if(user1.profile_privacy_type == "private") {
+
+        let follows = false;
+        try {
+          follows = await userFollowsUser(user.id, user_id);
+        } catch(e) {
+          return reject(new Error("ERROR_CHECKING_PERMISSION"));
+        }
+
+        if(!follows)
+          return reject(new Error("NOT_ALLOWED"));
+      }
     }
 
     let posts;
