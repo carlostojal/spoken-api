0305ed554313ba7b656855fbea84732710d02f29
Carlos Tojal <carlos.tojal@hotmail.com>
2021-01-06 00:08:58 +0000

    double looks better and is more accurate

--- src/resolvers/createPost.js
+++ src/resolvers/createPost.js
@@ -14,8 +14,8 @@ const createPost = (text, media_id, user) => {
     // remove spaces from begginig and end
     text = text.trim();
 
-    // replace consecutive multiple line breaks with single
-    text = text.replace(/\n\s*\n/g, '\n');
+    // replace consecutive multiple line breaks with double
+    text = text.replace(/\n\s*\n/g, '\n\n');
 
     if(text.length == 0 || text.match(process.env.EMPTY_POST_REGEX))
       return reject(new Error("INVALID_TEXT"));
