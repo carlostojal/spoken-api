b8bcee8508bd438e952bae6332dde6ec9a176c21
Carlos Tojal <carlos.tojal@hotmail.com>
2021-01-06 19:09:26 +0000

    logs

--- src/helpers/media/checkNsfw.js
+++ src/helpers/media/checkNsfw.js
@@ -17,6 +17,7 @@ const checkNsfw = (media) => {
     try {
       model = await nsfwjs.load(`${process.env.EXPRESS_ADDRESS}:${process.env.EXPRESS_PORT}/nsfw_model/`);
     } catch(e) {
+      console.error(e);
       return reject(new Error("ERROR_LOADING_MODEL"));
     }
 
