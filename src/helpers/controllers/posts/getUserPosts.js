ce5a90d684af288be16675dff1a6e3c691f84e0a
Carlos Tojal <carlos.tojal@hotmail.com>
2021-01-30 17:04:33 +0000

    simplify queries related to posts

--- src/helpers/controllers/posts/getUserPosts.js
+++ src/helpers/controllers/posts/getUserPosts.js
@@ -7,9 +7,14 @@ const getUserPosts = (page, per_page, user_id, mysqlPool) => {
       if(err)
         return reject(err);
 
-      connection.query(`SELECT Posts.id, Posts.time, Posts.text, Users.id AS poster_id, Users.name AS poster_name, 
-      Users.surname AS poster_surname, Users.username AS poster_username FROM Posts INNER JOIN Users ON
-      Posts.user_id = Users.ID WHERE Users.id = ? ORDER BY Posts.time DESC 
+      connection.query(`SELECT Posts.id, Posts.time, Posts.text, Posts.edited, (SELECT COUNT(*) FROM PostReactions WHERE post_id = Posts.id AND user_id = ?) AS user_reacted,
+      Users.id poster_id, Users.name AS poster_name, Users.surname AS poster_surname, Users.username AS poster_username,
+      Media.is_nsfw AS media_is_nsfw, Media.nsfw_cause AS media_nsfw_cause
+      FROM Posts 
+      INNER JOIN Users ON Posts.user_id = Users.id
+      LEFT JOIN Media ON Posts.media_id = Media.id
+      WHERE Posts.user_id = ?
+      ORDER BY Posts.time DESC
       LIMIT ? OFFSET ?`, [user_id, per_page, (page - 1) * per_page], async (err, result) => {
 
         connection.release();
--- src/helpers/formatPost.js
+++ src/helpers/formatPost.js
@@ -3,36 +3,13 @@ const formatPost = (post) => {
 
   return {
     id: post.id,
-    original_post: {
-      id: post.original_post_id,
-      poster: {
-        id: post.original_poster_id,
-        name: post.original_poster_name,
-        surname: post.original_poster_surname,
-        username: post.original_poster_username,
-        profile_pic: {
-          id: post.poster_profile_pic_media_id,
-          url: post.poster_profile_pic_media_id ? `${process.env.EXPRESS_ADDRESS}:${process.env.EXPRESS_PORT}/media/${post.original_poster_profile_pic_media_id}` : null,
-          is_nsfw: post.poster_is_nsfw,
-          nsfw_cause: post.poster_nsfw_cause
-        }
-      },
-      time: post.original_post_time,
-      text: post.original_post_text
-    },
     poster: {
       id: post.poster_id,
       name: post.poster_name,
       surname: post.poster_surname,
-      username: post.poster_username,
-      profile_pic: {
-        id: post.original_poster_profile_pic_media_id,
-        url: post.original_poster_profile_pic_media_id ? `${process.env.EXPRESS_ADDRESS}:${process.env.EXPRESS_PORT}/media/${post.original_poster_profile_pic_media_id}` : null,
-        is_nsfw: post.original_poster_is_nsfw,
-        nsfw_cause: post.original_poster_nsfw_cause
-      }
+      username: post.poster_username
     },
-    time: new Date(post.time).getTime(),
+    time: new Date(post.time),
     text: post.text,
     media: {
       id: post.media_id,
@@ -40,7 +17,6 @@ const formatPost = (post) => {
       is_nsfw: post.media_is_nsfw,
       nsfw_cause: post.media_nsfw_cause
     },
-    media_url: post.media_id ? `${process.env.EXPRESS_ADDRESS}:${process.env.EXPRESS_PORT}/media/${post.media_id}` : null,
     user_reacted: post.user_reacted,
     edited: post.edited == 1
   }
