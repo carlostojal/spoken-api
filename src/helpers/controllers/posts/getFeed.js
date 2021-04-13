bd0b010dc4d4e8ed230d3a9e0db9d5b7d8b19b75
Carlos Tojal <carlos.tojal@hotmail.com>
2021-04-13 21:23:13 +0100

    implement mysql procedure for feed call

--- src/helpers/controllers/posts/getFeed.js
+++ src/helpers/controllers/posts/getFeed.js
@@ -9,23 +9,14 @@ const getFeed = (page, perPage, user_id, mysqlPool) => {
       if(err)
         return reject(err);
 
-      connection.query(`SELECT Posts.id, Posts.time, Posts.text, Posts.edited, (SELECT COUNT(*) FROM PostReactions WHERE post_id = Posts.id AND user_id = ?) AS user_reacted,
-      Users.id poster_id, Users.name AS poster_name, Users.surname AS poster_surname, Users.username AS poster_username,
-      Media.id AS media_id, Media.is_nsfw AS media_is_nsfw, Media.nsfw_cause AS media_nsfw_cause
-      FROM Posts 
-      INNER JOIN Users ON Posts.user_id = Users.id
-      LEFT JOIN Media ON Posts.media_id = Media.id
-      LEFT JOIN FollowRelations ON FollowRelations.user = ? AND FollowRelations.follows = Posts.user_id AND FollowRelations.accepted = '1'
-      WHERE FollowRelations.user IS NOT NULL OR Posts.user_id = ?
-      ORDER BY Posts.time DESC
-      LIMIT ? OFFSET ?`, [user_id, user_id, user_id, perPage, (page - 1) * perPage], async (err, result) => {
+      connection.query(`CALL GetFeed(?, ?, ?)`, [user_id, perPage, (page - 1) * perPage], async (err, result) => {
 
         connection.release();
   
         if(err)
           return reject(err);
   
-        result = JSON.parse(JSON.stringify(result));
+        result = JSON.parse(JSON.stringify(result))[0];
   
         /*
         result.map(async (post) => {
