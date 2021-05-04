8fe3232ce4bb63e9233cd6319aa136c894a22424
Carlos Tojal <carlos.tojal@hotmail.com>
2021-05-05 00:10:32 +0100

    capture mutation done. not tested yet.

---
+++ src/helpers/controllers/analytics/captureUsageData.js
@@ -0,0 +1,23 @@
+
+const captureUsageData = (user_id, post_id, user_lat, user_long, user_os, view_time, mysqlPool) => {
+  return new Promise((resolve, reject) => {
+
+    mysqlPool.getConnection((err, connection) => {
+
+      if(err)
+        return reject(err);
+
+      connection.query("INSERT INTO UsageData (user_id, post_id, user_lat, user_long, user_os, view_time) VALUES" + 
+        "(?, ?, ?, ?, ?, ?)", [user_id, post_id, user_lat, user_long, user_os, view_time], (err, result) => {
+
+          if(err)
+            return reject(err);
+
+
+          return resolve(null);
+        })
+    });
+  });
+};
+
+module.exports = captureUsageData;
--- src/resolvers/captureUsageData.js
+++ src/resolvers/captureUsageData.js
@@ -1,4 +1,5 @@
 const { AuthenticationError } = require("apollo-server");
+const captureDataController = require("../helpers/controllers/analytics/captureUsageData");
 
 const captureUsageData = (post_id, user_lat, user_long, user_os, view_time, user, mysqlPool) => {
   return new Promise((resolve, reject) => {
@@ -17,6 +18,15 @@ const captureUsageData = (post_id, user_lat, user_long, user_os, view_time, user
       - view_time
       
     */
+
+    try {
+      // will not await because data collection is not critical to make the user wait for it
+      captureDataController(user.id, post_id, user_lat, user_long, user_os, view_time, mysqlPool);
+    } catch(e) {
+      return reject(new Error("ERROR_CAPTURING_DATA"));
+    }
+
+    return resolve(null)
   });
 };
 
