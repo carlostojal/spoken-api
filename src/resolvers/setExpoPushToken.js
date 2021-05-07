4c4b9936eed89e8c6ac96df7a1c6d53a1b07b6e0
Carlos Tojal <carlos.tojal@hotmail.com>
2021-05-07 15:01:31 +0100

    set push token done

--- src/resolvers/setExpoPushToken.js
+++ src/resolvers/setExpoPushToken.js
@@ -1,17 +1,25 @@
 const { AuthenticationError } = require("apollo-server");
+const User = require("../db_models/User");
 
-const setPushToken = require("../helpers/controllers/users/setPushToken");
-
-const setExpoPushToken = (token, user, mysqlPool) => {
+const setExpoPushToken = (token, user) => {
   return new Promise(async (resolve, reject) => {
 
     if(!user)
       return reject(new AuthenticationError("BAD_AUTHENTICATION"));
       
+    let cur_user = null;
     try {
-      await setPushToken(user.id, token, mysqlPool);
+      cur_user = await User.findById(user._id);
     } catch(e) {
-      return reject(e);
+      console.error(e);
+      return reject(new Error("ERROR_GETTING_USER"));
+    }
+
+    try {
+      user.push_token = token;
+      await user.save();
+    } catch(e) {
+      return reject(new Error("ERROR_UPDATING_TOKEN"));
     }
 
     return resolve(true);
